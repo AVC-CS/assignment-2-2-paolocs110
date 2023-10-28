5598ef7509716bf370df172cd9c636925d329216
paolocs110 <pciappetta@students.avc.edu>
2023-10-27 23:14:37 -0700

    complete assignment 2

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
+    celsius = int(input('Enter yout tempeture in Celsius: '))
+    fahrenheit = 9 / 5 * celsius + 32
 
+    print (f'The converted temperature is {fahrenheit:.2f}')
     """
     ########################################
     # Do not delete the return statement
